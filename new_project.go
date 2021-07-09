88c149dcf3f6bd1459f3db2358dfc2450e6f027e
pengincoalition <kage2.kage22@gmail.com>
2021-07-09 12:33:15 +0900

    Use default font and change the program of new pro

--- new_project.go
+++ new_project.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func make_file(fname string, text []byte) {
@@ -24,20 +25,25 @@ func file_load(fileName string) []byte {
 }
 
 func new_project() {
-	make_file("./main.aasc", []byte(""))
+
+	if _, err := os.Stat("setting.json"); err == nil {
+		log.Fatalln("The directory in which make a project must be empty.")
+	}
+
+	make_file("main.aasc", []byte(""))
 	text, _ := json.Marshal(new_setting_file_t())
-	make_file("./setting.json", (text))
-	os.Mkdir("./font", 0777)
+	make_file("setting.json", (text))
+	os.Mkdir("font", 0777)
 
-	/*
-		my_out_path, err := os.Executable()
+	my_out_path, err := os.Executable()
 
-		if err != nil {
-			log.Fatalln(err)
-		}
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-		my_dir_path := filepath.Dir(my_out_path)
+	my_dir_path := filepath.Dir(my_out_path)
 
-		make_file("./font/font.ttf", file_load(filepath.Join(my_dir_path, "font", "font.ttf")))
-	*/
+	make_file(filepath.Join("font", "font.ttf"), file_load(filepath.Join(my_dir_path, "default_font", "font.ttf")))
+	make_file(filepath.Join("font", "IPA_Font_License_Agreement_v1.0.txt"), file_load(filepath.Join(my_dir_path, "default_font", "IPA_Font_License_Agreement_v1.0.txt")))
+	make_file(filepath.Join("font", "Readme_ipam00303.txt"), file_load(filepath.Join(my_dir_path, "default_font", "Readme_ipam00303.txt")))
 }
